c83b29b3956117ac5f6a3263fbadc7027194b76f
Unknown <moreggmoreskill@icloud.com>
2018-05-20 14:51:54 +0300

    Added TimeGuard and TickGuard (Win) classes
    Now there are two more ways to measure time in program. New classes provide convenient way how to measure time in more procedural styled programs, when there are no opportunily (or no sence) to create functional object, Now, instead of "Callable" object you can just create new scope, and pass reference to accumulator value when initializing.

--- TimeMeasurer.hpp
+++ TimeMeasurer.hpp
@@ -13,6 +13,18 @@
 
 enum Ratio {nanoseconds, microseconds, milliseconds, seconds};
 
+//this constexpr function's use is to set up
+//ratio template for "std::ratio <>"
+static constexpr unsigned long long _ratio_to_ll(const Ratio r)
+{
+    switch(r) {
+    case nanoseconds:  return 1000000000ll;
+    case microseconds: return 1000000ll;
+    case milliseconds: return 1000ll;
+    case seconds:      return 1ll;
+    }
+}
+
 template <Ratio ratio>
 class TimeMeasurer
 {
@@ -36,6 +48,7 @@ public:
     //function's template starts new thread, which will execute Callable object, passing "args".
     //returns "std::future <double>" object which will store time value depended on ratio template argument.
 
+	/*
     template <class Callable, class ...Args>
     static std::future <long double> async_measure_exec_time(Callable f, Args... args)
     {
@@ -46,21 +59,7 @@ public:
             auto stop = std::chrono::high_resolution_clock::now();
             return std::chrono::duration <long double, std::ratio <1, _ratio_to_ll(ratio)>>(stop - start).count();
         });
-    }
-
-private:
-    //this constexpr function's use is to set up
-    //ratio template for "std::ratio <>"
-
-    static constexpr unsigned long long _ratio_to_ll(const Ratio r)
-    {
-        switch(r) {
-        case nanoseconds:  return 1000000000ll;
-        case microseconds: return 1000000ll;
-        case milliseconds: return 1000ll;
-        case seconds:      return 1ll;
-        }
-    }
+    }*/
 };
 
 #ifdef _WIN32
@@ -76,6 +75,55 @@ namespace TickMeasurer
         return stop.QuadPart - start.QuadPart;
     }
 }
+#endif
+
+//this class uses it's existance time in code to measure time
+template <Ratio ratio>
+class TimeGuard
+{
+public:
+	TimeGuard(long double &dest) noexcept
+		: start(std::chrono::high_resolution_clock::now()), out(dest)
+	{}
+	TimeGuard(const TimeGuard &) = delete;
+	TimeGuard(TimeGuard &&) = delete;
+	operator =(const TimeGuard &) = delete;
+	
+	~TimeGuard()
+	{
+		auto stop = std::chrono::high_resolution_clock::now();
+		out = std::chrono::duration <long double, std::ratio <1, _ratio_to_ll(ratio)>>(stop - start).count();
+	}
+	
+private:
+	std::chrono::time_point <std::chrono::high_resolution_clock> start;
+	long double &out;
+};
+
+#ifdef _WIN32
+class TickGuard
+{
+public:
+	TickGuard(uint64_t &dest) noexcept
+		: out(dest)
+	{
+		QueryPerformanceCounter(&start);
+	}
+	TickGuard(const TickGuard &) = delete;
+	TickGuard(TickGuard &&) = delete;
+	operator =(const TickGuard &) = delete;
+	
+	~TickGuard()
+	{
+		LARGE_INTEGER stop;
+		QueryPerformanceCounter(&stop);
+		out = stop.QuadPart - start.QuadPart;
+	}
+	
+private:
+	LARGE_INTEGER start;
+	uint64_t &out;
+};
 #endif
 
 #endif // MEASUREEXECTIME_H
